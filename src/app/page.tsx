cb1871c69c34c014b1d43ffef708e3b133441b2b
arw6329 <70402863+arw6329@users.noreply.github.com>
2025-11-16 10:05:19 -0800

    Fix errors preventing build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 
-import { FamilyTree } from "@reunionpage/react-family-tree"
 import styles from "./page.module.css"
 import familyTree from "#/lib/familyTree.json"
+// TODO: Better way to import database type
+import type { FamilyTreeDatabase } from "@reunionpage/react-family-tree/dist/lib/family-tree/FamilyTreeDatabase"
+import dynamic from 'next/dynamic'
+ 
+const FamilyTree = dynamic(
+  () => import('@reunionpage/react-family-tree').then(mod => mod.FamilyTree),
+  { ssr: false }
+)
 
 export default function Home() {
     return (
@@ -12,7 +19,7 @@ export default function Home() {
                     <h1 className={styles.h1}>ReunionPage Family Tree Demo - Edge Build</h1>
                     <span>See <a href="https://github.com/arw6329/family-tree-renderer">https://github.com/arw6329/family-tree-renderer</a> for more info</span>
                 </div>
-                <FamilyTree database={familyTree} />
+                <FamilyTree database={familyTree as FamilyTreeDatabase} />
             </main>
         </div>
     )
